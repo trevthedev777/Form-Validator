7cd27653f01c2c8c325f782824aaa358ea150798
Your Name <you@example.com>
2022-06-06 12:54:55 -0400

    Create function to display error message

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+// Global Variables
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+/* Shows The Input Error Message*/
+// Takes in whatever the input is, and shows the message
+function showError(input, message) {
+    // Input Border Change
+    const formControl = input.parentElement;
+    formControl.className = "form-control error";
+    // small tag error message
+    const small = formControl.querySelector("small");
+    small.innerHTML = message;
+}
+
+// Event Listeners
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    if (username.value === '') {
+        showError(username, 'Username is required');
+        console.log(showError);
+    } else {
+        showSuccess(username);
+        console.log(showSuccess);
+    }
+});

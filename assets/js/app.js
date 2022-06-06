5e21e1c5edd67a00ba683576e7f72014fd9cf0d8
Your Name <you@example.com>
2022-06-06 15:17:33 -0400

    Add form validation

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,15 +23,51 @@ function showSuccess(input) {
     formControl.className = "form-control success";
 };
 
+/* Validates Email Address*/
+function emailIsValid(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 // Event Listeners
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
+    // Username Validation
     if (username.value === '') {
         showError(username, 'Username is required');
         console.log(showError);
     } else {
         showSuccess(username);
         console.log(showSuccess);
-    }
+    };
+
+    // Email Validation
+    if (email.value === '') {
+        showError(email, 'Email address is required');
+        console.log(showError);
+    } else if (!emailIsValid(email.value)){
+        showError(email, 'Email is not valid');
+    } else {
+        showSuccess(email);
+        console.log(showSuccess);
+    };
+
+    // Password Validation
+    if (password.value === '') {
+        showError(password, 'Password is required');
+        console.log(showError);
+    } else {
+        showSuccess(password);
+        console.log(showSuccess);
+    };
+
+    // Confirm Password validation
+    if (password2.value === !password.value) {
+        showError(password2, 'Passwords do not match');
+        console.log(showError);
+    } else {
+        showSuccess(password2);
+        console.log(showSuccess);
+    };
 });
